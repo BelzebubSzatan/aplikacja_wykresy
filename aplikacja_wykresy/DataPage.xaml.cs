63debb7b294df44780bf7504101de0cddf21047b
KacperMatlag <kacpermatlag@gmail.com>
2024-01-17 11:39:26 +0100

    add animations

--- aplikacja_wykresy/DataPage.xaml.cs
+++ aplikacja_wykresy/DataPage.xaml.cs
@@ -12,7 +12,7 @@ namespace aplikacja_wykresy
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DataPage : ContentPage
     {
-        List<GraphElement> elements=new List<GraphElement>();
+        List<GraphElement> elements = new List<GraphElement>();
         public DataPage()
         {
             InitializeComponent();
@@ -20,16 +20,26 @@ namespace aplikacja_wykresy
         protected override void OnAppearing()
         {
             base.OnAppearing();
+
+            if (App.Elements.Count > 0)
+            {
+                for (int i = 0; i < App.Elements.Count; i++)
+                {
+                    List<Entry> entries = DataGrid.Children.OfType<Entry>().Where(z => z.ClassId == i.ToString()).ToList();
+                    entries[0].Text = App.Elements[i].Name;
+                    entries[1].Text = App.Elements[i].Value.ToString();
+                }
+            }
         }
         private async void ChangeData_Clicked(object sender, EventArgs e)
         {
             App.Elements.Clear();
-            for(int i = 0; i < 7; ++i)
+            for (int i = 0; i < 7; ++i)
             {
-                List<Entry> entries=DataGrid.Children.OfType<Entry>().Where(z=>z.ClassId==i.ToString()).ToList();
+                List<Entry> entries = DataGrid.Children.OfType<Entry>().Where(z => z.ClassId == i.ToString()).ToList();
                 if (entries.Count == 2)
                 {
-                    if (entries[0].Text !=null && entries[1].Text !=null)
+                    if (entries[0].Text != null && entries[1].Text != null)
                     {
                         elements.Add(new GraphElement()
                         {
--- aplikacja_wykresy/MainPage.xaml.cs
+++ aplikacja_wykresy/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace aplikacja_wykresy
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : TabbedPage
     {
-        List<Color> colors=new List<Color>() { 
+        List<Color> colors = new List<Color>() {
             Color.Green, Color.Blue,
             Color.Red, Color.Magenta,
             Color.Yellow,Color.Orange,
@@ -30,15 +30,16 @@ namespace aplikacja_wykresy
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
             if (App.Elements.Count == 0) return;
 
+            BarGraph.Children.Clear();
             Name.Text = App.Name;
+            BarGraph.ColumnDefinitions.Clear();
 
-            for(int i=0;i<App.Elements.Count;++i)
+            for (int i = 0; i < App.Elements.Count; ++i)
                 BarGraph.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
 
-            double maxValue=App.Elements.Max(x => x.Value);
+            double maxValue = App.Elements.Max(x => x.Value);
 
             for (int i = 0; i < App.Elements.Count; i++)
             {
@@ -47,16 +48,18 @@ namespace aplikacja_wykresy
                 container.VerticalOptions = LayoutOptions.End;
                 StackLayout graphElement = new StackLayout()
                 {
-                    HeightRequest = App.Elements[i].Value/maxValue*500,
+                    HeightRequest = App.Elements[i].Value / maxValue * 500,
                     BackgroundColor = colors[i],
                     VerticalOptions = LayoutOptions.End,
+                    ScaleY = 0,
+                    AnchorY = 1
                 };
                 graphElement.Children.Add(new Label()
                 {
                     Text = App.Elements[i].Value.ToString(),
                     VerticalOptions = LayoutOptions.Start,
-                    TextColor=Color.White,
-                    HorizontalTextAlignment=TextAlignment.Center,
+                    TextColor = Color.White,
+                    HorizontalTextAlignment = TextAlignment.Center,
                 });
                 container.Children.Add(graphElement);
                 container.Children.Add(new Label()
@@ -64,10 +67,11 @@ namespace aplikacja_wykresy
                     Text = App.Elements[i].Name,
                     FontSize = 20,
                     TextColor = Color.Black,
-                    HorizontalTextAlignment=TextAlignment.Center,
+                    HorizontalTextAlignment = TextAlignment.Center,
                 });
-                Grid.SetColumn(container,i);
+                Grid.SetColumn(container, i);
                 BarGraph.Children.Add(container);
+                graphElement.ScaleYTo(1, 2000, Easing.SinInOut);
             }
 
         }
